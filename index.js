b5ea2ca443f59189bfc36e82fb3ac0cde23abd4c
NotFaizen <83504931+NotFaizen@users.noreply.github.com>
2022-07-25 13:28:50 +0600

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ module.exports={
 		console.log('go after dreams not people');
 	},
 	love:function(){
-
+		console.log('love yourself instead of some imaginary girl')
+	},
+	cheatOn:function(){
+		console.log("you've got some nerve cheating on your non-existant girlfriend")
+	},
+	getMarried:function(){
+		console.log("you and your imaginary girlfriend get married, now she's your imaginary wife")
 	}
 }
